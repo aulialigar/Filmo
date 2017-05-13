7370b1c735cb7f9f72e7b0874fe9f79b1e138f53
aulialigar <aulialsh69@gmail.com>
2017-05-13 21:09:28 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl409/filmo/model/SourcesResponse.java
@@ -0,0 +1,12 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl409.filmo.model;
+
+import java.util.List;
+
+/**
+ * Created by Smktelkom on 5/13/2017.
+ */
+
+public class SourcesResponse {
+    public String status;
+    public List<Result> results;
+}
